b037cd54c0e575170b252394316656b3d68779ba
pan_huajian <42933@dahuatech.com>
2018-11-30 11:34:29 +0800

    login redict to problem

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,17 +1,20 @@
 <template>
     <div class="login-container">
+        <div class="login-bg" :class="[bgnth]"></div>
         <el-form class="login-form" status-icon
          ref="loginForm" 
         :rules="rules" 
         :model="loginForm" >
+            <div class="form-bg"></div>
+            <p class="title"><i class="logo"></i><span>大华视频物联能力开放平台后台管理</span></p>
             <el-form-item prop="name">
                 <el-input v-model="loginForm.name"><i slot="prefix" class="iconfont icon-zhanghu"></i></el-input>
             </el-form-item>
             <el-form-item prop="password">
-                <el-input v-model="loginForm.password"><i slot="prefix" class="iconfont icon-mima"></i></el-input>
+                <el-input @keyup.enter.native="submitForm('loginForm')" v-model="loginForm.password"><i slot="prefix" class="iconfont icon-mima"></i></el-input>
             </el-form-item>
             <el-form-item>
-                <el-button class="submit-btn" type="primary" @click="submitForm('loginForm')">SUBMIT</el-button>
+                <el-button class="submit-btn" type="primary" @keyup.enter.native="submitForm('loginForm')" @click="submitForm('loginForm')">登录</el-button>
                 <!-- <el-button type="primary" @click="resetForm('loginForm')">SUBMIT</el-button> -->
             </el-form-item>
         </el-form>
@@ -37,7 +40,9 @@ export default {
                     { required: true, message: 'password required', trigger: 'blur' },
                     { min: 3, max: 16, message: 'limit in 3-16', trigger: 'blur' }
                 ]
-            }
+            },
+            bgnth: 'bg-nth0',
+            bgindex: 0
         }
     },
     methods: {
@@ -67,11 +72,18 @@ export default {
                 }
             }
             this.$store.dispatch('login', params).then(() => {
-                this.$router.push('/homeConfig');
+                this.$router.push('/demand/problem');
             }).catch(e => {
 
             })
         }
+    },
+    mounted () {
+        setInterval( () => {
+            this.bgindex ++;
+            if(this.bgindex == 3) this.bgindex = 0;
+            this.bgnth = 'bg-nth' + this.bgindex
+        },4000)
     }
 }
 </script>
@@ -88,24 +100,66 @@ export default {
 <style lang="less" scoped>
 .login-container {
     position: absolute;
-    background-color: rgb(49, 105, 138);
     width: 100%;
     height: 100%;
     .login-form {
         position: absolute;
-        width: 20%;
-        min-width: 100px;
+        width: 400px;
         top: 50%;
         left: 50%;
         transform: translate(-50%, -50%);
+        padding: 30px 50px 30px 50px;
         .iconfont {
             font-size: 18px;
             color: #aaa;
             padding-left: 7px;
         }
+        .form-bg {
+            position: absolute;
+            top: 0; left: 0;
+            width: 100%; height: 100%;
+            background-color: #fff;
+            border-radius: 5px;
+            opacity: .75;
+            z-index: -1;
+        }
+        .title {
+            color: #333;
+            font-size: 22px;
+            i {
+                display: inline-block;
+                width: 38px;
+                height: 38px;
+                vertical-align: middle;
+                background-image: url('../assets/img/logo.png');
+                background-repeat: no-repeat;
+                background-position: center;
+                background-size: 36px 36px;
+            }
+        }
     }
     .submit-btn {
         width: 100%;
     }
+    .login-bg {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-repeat: no-repeat;
+        background-size: 100% 100%;
+        background-position: 0px 0px;
+        transition: all 1000ms;
+    }
+}
+.bg-nth0 {
+    background-image: url('../assets/img/bg0.jpg')
+}
+.bg-nth1 {
+    background-image: url('../assets/img/bg1.jpg')
+}
+.bg-nth2 {
+    background-image: url('../assets/img/bg2.jpg')
 }
 </style>
