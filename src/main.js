8569bd955a74a373e3c2120b4c87507a07da419e
pan_huajian <42933@dahuatech.com>
2018-12-18 19:22:21 +0800

    2132109

--- src/main.js
+++ src/main.js
@@ -9,8 +9,12 @@ import 'element-ui/lib/theme-chalk/index.css'
 import './styles/iconfont.css'
 
 Vue.config.productionTip = false;
-// axios.defaults.baseURL = 'http://10.35.125.21:8999/openeco'
-axios.defaults.baseURL = 'http://33.255.11.245:8999/openeco'
+if(window.location.host.indexOf('10.35.125.21')>-1) {
+  axios.defaults.baseURL = 'http://10.35.125.21:8999/openeco'
+} else if (window.location.host.indexOf('open.dahuatech.com')>-1) {
+  axios.defaults.baseURL = 'http://open.dahuatech.com:8081/api/openeco'
+}
+// axios.defaults.baseURL = 'http://33.255.11.245:8999/openeco'
 axios.defaults.withCredentials=false;
 Vue.prototype.$axios = axios
 
